f81984cac3cc44bba4d8407d361f78ff90bbb184
lei <lei@qq.com>
2017-10-19 15:12:20 +0800

    'cmdb'

--- cmdb/models.py
+++ cmdb/models.py
@@ -3,41 +3,40 @@ from django.db import models
 from serversys.models import *
 
 # Create your models here.
-class Account(models.Model):
-    SN = models.CharField("序列号",max_length=30)
+class account(models.Model):
     Business = models.CharField("所属业务",max_length=100)
-    IP = models.GenericIPAddressField("公网IP",blank=True,null=True)
-    Inner_IP = models.GenericIPAddressField("内网IP",blank=True,null=True)
-    User = models.CharField("用户名",max_length=30)
-    Passwd = models.CharField("密码",max_length=200)
-    Port = models.CharField("端口",max_length=30)
-    Landing_mode = models.CharField("登陆方式",max_length=30)
-    Create_Date = models.DateTimeField('创建时间')
+    ip = models.GenericIPAddressField("公网IP",blank=True,null=True)
+    inner_ip = models.GenericIPAddressField("内网IP",blank=True,null=True)
+    user = models.CharField("用户名",max_length=30)
+    passwd = models.CharField("密码",max_length=200)
+    port = models.CharField("端口",max_length=30)
+    landing_mode = models.CharField("登陆方式",max_length=30)
+    create_date = models.DateTimeField('创建时间')
 
     def __unicode__(self):
         return self.SN
 
 class cmdb(models.Model):
-    SN = models.CharField('序列号',max_length=30)
-    CPU = models.CharField('CPU',max_length=100)
-    Fan = models.CharField('风扇',max_length=100)
-    Motherboard = models.CharField('主板',max_length=100)
-    Memory = models.CharField('内存条',max_length=100)
-    Graphics = models.CharField('显卡',max_length=100)
-    Hard_disk1 = models.CharField('硬盘1',max_length=100)
-    Keyboard = models.CharField('键盘/鼠标',max_length=100)
-    Chassis = models.CharField('机箱',max_length=100)
-    Power_Supply = models.CharField('电源',max_length=100)
-    Monitor = models.CharField('显示器',max_length=100)
-    Who_uses = models.CharField('使用者',max_length=100)
-    Price = models.IntegerField('采购价',max_length=100)
-    Supplier = models.ForeignKey('Group_Supplier')         #供应商
+    cpu = models.CharField('CPU',max_length=100)
+    fan = models.CharField('风扇',max_length=100)
+    motherboard = models.CharField('主板',max_length=100)
+    memory = models.CharField('内存条',max_length=100)
+    graphics = models.CharField('显卡',max_length=100)
+    hard_disk1 = models.CharField('硬盘1',max_length=100)
+    keyboard = models.CharField('键盘/鼠标',max_length=100)
+    chassis = models.CharField('机箱',max_length=100)
+    power_Supply = models.CharField('电源',max_length=100)
+    monitor = models.CharField('显示器',max_length=100)
+    who_uses = models.CharField('使用者',max_length=100)
+    price = models.IntegerField('采购价',max_length=100)
+    supplier = models.ForeignKey('Group_Supplier')         #供应商
     dept = models.ForeignKey(Dept, null=True, blank=True)  #部门
-    Create_Date = models.DateTimeField('创建时间')
+    create_Date = models.DateTimeField('创建时间')
+    comment = models.TextField(blank=True, null=True,max_length=500)
 
     def __unicode__(self):
         return self.SN
 
-class Group_Supplier(models.Model):
-    Supplier_name = models.CharField('供应商',max_length=100)
-    Phone = models.IntegerField('联系方式',max_length=100)
+class group_supplier(models.Model):
+    supplier_name = models.CharField('供应商',max_length=100)
+    phone = models.IntegerField('联系方式',max_length=100)
--- cmdb/views.py
+++ cmdb/views.py
@@ -28,7 +28,7 @@ def cmdb_add(request):
     if request.method == "GET":
         Username = request.session.get('user_name')
         Deptdata = Dept.objects.all()
-        Supplier = Group_Supplier.objects.all()
+        Supplier = group_supplier.objects.all()
 
         kwvars = {
             'request':request,
@@ -38,4 +38,28 @@ def cmdb_add(request):
         }
 
         return render_to_response('cmdb/cmdb_add.html',kwvars,RequestContext(request))
+
+    if request.method == "POST":
+        cpu = request.POST.get('cpu')
+        fan = request.POST.get('fan')
+        motherboard = request.POST.get('motherboard')
+        memory = request.POST.get('memory')
+        graphics = request.POST.get('graphics')
+        dept_id = request.POST.get('dept')
+        deptname = Dept.objects.get(id=dept_id)
+        hard_disk1 = request.POST.get('hard_disk1')
+        keyboard = request.POST.get('keyboard')
+        chassis = request.POST.get('chassis')
+        power_supply = request.POST.get('power_Supply')
+        monitor = request.POST.get('monitor')
+        who_uses = request.POST.get('who_uses')
+        price = request.POST.get('price')
+        supplier_id = request.POST.get('supplier')
+        supplier = group_supplier.objects.get(id=supplier_id)
+        datetime = request.POST.get('datetime')
+        description = request.POST.get('description')
+
+        p=cmdb(Cpu=cpu,Fan=fan,Motherboard=motherboard,Memory=memory,Graphics=graphics,Hard_disk1=hard_disk1,Keyboard=keyboard,
+               Chassis=chassis,Power_Supply=power_supply,Monitor=monitor,Who_uses=who_uses,Price=price,Supplier=supplier,dept=deptname,comment=description,Create_Date=datetime)
+
 
