62c84bf751f7a39562babe95b5a6cc9aaaa5a9d7
lei <lei@qq.com>
2017-10-25 20:36:31 +0800

    'cmdb'

--- cmdb/views.py
+++ cmdb/views.py
@@ -125,7 +125,7 @@ def supplier_edit(request,sid):
 
     if request.method == 'POST':
 
-        print "----->",type(request.POST),request.POST['supplier_name']
+        print "----->",type(request.POST)
         Getsupp_name = request.POST.get('supplier_name')
         # Getsupp_phone = request.POST.get('supplier_phone')
 
